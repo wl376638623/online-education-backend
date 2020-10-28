d9bb2cb6d428d71f50f2e4e81b63ca1003508a18
wanglu <376638623@qq.com>
2020-10-28 09:08:26 +0900

    '环境配置'

---
+++ service/service_edu/src/main/java/com/wanglu/eduservice/mapper/EduTeacherMapper.java
@@ -0,0 +1,16 @@
+package com.wanglu.eduservice.mapper;
+
+import com.wanglu.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-02-24
+ */
+public interface EduTeacherMapper extends BaseMapper<EduTeacher> {
+
+}
---
+++ service/service_edu/src/main/java/com/wanglu/eduservice/service/impl/EduTeacherServiceImpl.java
@@ -0,0 +1,20 @@
+package com.wanglu.eduservice.service.impl;
+
+import com.wanglu.eduservice.entity.EduTeacher;
+import com.wanglu.eduservice.mapper.EduTeacherMapper;
+import com.wanglu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-02-24
+ */
+@Service
+public class EduTeacherServiceImpl extends ServiceImpl<EduTeacherMapper, EduTeacher> implements EduTeacherService {
+
+}
