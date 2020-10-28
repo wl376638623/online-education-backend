be4ce30c73f0f7a98487ac65128b8135607ced59
wanglu <376638623@qq.com>
2020-10-28 15:09:05 +0900

    '统一异常处理'

--- service/service_edu/src/main/java/com/wanglu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/wanglu/eduservice/controller/EduTeacherController.java
@@ -76,60 +76,59 @@ public class EduTeacherController {
     }
 
     //4 条件查询带分页的方法
-    @PostMapping("pageTeacherCondition/{current}/{limit}")
-    public R pageTeacherCondition(@PathVariable long current,@PathVariable long limit,
-                                  @RequestBody(required = false)  TeacherQuery teacherQuery) {
-        //创建page对象
-        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+   @PostMapping("pageTeacherCondition/{current}/{limit}")
+   public R pageTeacherCondition(@PathVariable long current,@PathVariable long limit,
+                                 @RequestBody(required = false) TeacherQuery teacherQuery){
+        //创建一个page对象
+       Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+       //构造条件
+       QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+       //wrapper多条件组合查询
+       //判断条件值是否为空，如果不为空拼接
+       String name = teacherQuery.getName();
+       Integer level = teacherQuery.getLevel();
+       String begin = teacherQuery.getBegin();
+       String end = teacherQuery.getEnd();
+       if (!StringUtils.isEmpty(name)) {
+           //构建条件
+           wrapper.like("name", name);
+       }
+       if (!StringUtils.isEmpty(level)) {
+           wrapper.eq("level", level);
+       }
+       if (!StringUtils.isEmpty(begin)) {
+           wrapper.ge("gmt_create", begin);
+       }
+       if (!StringUtils.isEmpty(end)) {
+           wrapper.le("gmt_modified", end);
+       }
+       //调用方法实现条件查询
+       teacherService.page(pageTeacher, wrapper);
+       List<EduTeacher> records = pageTeacher.getRecords();//数据list
+       long total = pageTeacher.getTotal();//总记录数
+       return R.ok().data("total", total).data("rows", records);
 
-        //构建条件
-        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
-       // 多条件组合查询
-        // mybatis学过 动态sql
-        String name = teacherQuery.getName();
-        Integer level = teacherQuery.getLevel();
-        String begin = teacherQuery.getBegin();
-        String end = teacherQuery.getEnd();
-        //判断条件值是否为空，如果不为空拼接条件
-        if(!StringUtils.isEmpty(name)) {
-            //构建条件
-            wrapper.like("name",name);
-        }
-        if(!StringUtils.isEmpty(level)) {
-            wrapper.eq("level",level);
-        }
-        if(!StringUtils.isEmpty(begin)) {
-            wrapper.ge("gmt_create",begin);
-        }
-        if(!StringUtils.isEmpty(end)) {
-            wrapper.le("gmt_create",end);
-        }
+   }
 
-        //调用方法实现条件查询分页
-        teacherService.page(pageTeacher,wrapper);
-
-        long total = pageTeacher.getTotal();//总记录数
-        List<EduTeacher> records = pageTeacher.getRecords(); //数据list集合
-        return R.ok().data("total",total).data("rows",records);
-    }
 
     //添加讲师接口的方法
     @PostMapping("addTeacher")
     public R addTeacher(@RequestBody EduTeacher eduTeacher) {
         boolean save = teacherService.save(eduTeacher);
-        if(save) {
-            return R.ok();
-        } else {
-            return R.error();
-        }
+//        if (save) {
+////            return R.ok();
+////        } else {
+////            return R.error();
+////        }
+        return save ? R.ok() : R.error();
     }
 
     //根据讲师id进行查询
-    @GetMapping("getTeacher/{id}")
-    public R getTeacher(@PathVariable String id) {
-        EduTeacher eduTeacher = teacherService.getById(id);
-        return R.ok().data("teacher",eduTeacher);
-    }
+   @GetMapping("getTeacher/{id}")
+   public R getTeacher(@PathVariable String id){
+       EduTeacher eduTeacher = teacherService.getById(id);
+       return R.ok().data("teacher", eduTeacher);
+   }
 
     //讲师修改功能
     @PostMapping("updateTeacher")
