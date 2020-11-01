1d5e344429f75ac6351e9635dc45ef1433679611
wanglu <376638623@qq.com>
2020-11-02 00:36:26 +0900

    '读取配置文件数值'

---
+++ service/service_oss/src/main/java/com/wanglu/oss/utils/ConstandPropertiesUtils.java
@@ -0,0 +1,30 @@
+package com.wanglu.oss.utils;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConstandPropertiesUtils implements InitializingBean {
+    //读取配置文件的内容
+    @Value("${aliyun.oss.file.endpoint}")
+    private String endPoint;
+    @Value("${aliyun.oss.file.keyid}")
+    private String keyId;
+    @Value("${aliyun.oss.file.keysecret}")
+    private String keySecret;
+    @Value("${aliyun.oss.file.bucketname}")
+    private String bucketName;
+
+    public static String END_POINT;
+    public static String ACCESS_KEY_ID;
+    public static String ACCESS_KEY_SECRET;
+    public static String BUCKETNAME;
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        END_POINT = endPoint;
+        ACCESS_KEY_ID = keyId;
+        ACCESS_KEY_SECRET = keySecret;
+        BUCKETNAME = bucketName;
+    }
+}
