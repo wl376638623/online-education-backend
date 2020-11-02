1400acd0c8238e172ec392f91be46a51037b27a5
wanglu <376638623@qq.com>
2020-11-02 14:59:23 +0900

    '完成uuid和随着日期生成文件夹代码完成'

--- service/service_oss/src/main/java/com/wanglu/oss/service/impl/OssServiceImpl.java
+++ service/service_oss/src/main/java/com/wanglu/oss/service/impl/OssServiceImpl.java
@@ -4,11 +4,13 @@ import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
 import com.wanglu.oss.service.OssService;
 import com.wanglu.oss.utils.ConstandPropertiesUtils;
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.UUID;
 
 @Service
 public class OssServiceImpl implements OssService {
@@ -27,6 +29,16 @@ public class OssServiceImpl implements OssService {
             InputStream inputStream = file.getInputStream();
             //获取文件名称
             String filename = file.getOriginalFilename();
+            //在文件名称里面加入随机唯一值 让其不重复
+            String uuid = UUID.randomUUID().toString();
+            //去除-
+            uuid.replaceAll("-", "");
+            filename = uuid + filename;
+            //把文件按照日期进行分类
+            //获取当前日期用joda-time
+            String datePath = new DateTime().toString("yyyy/MM/dd");
+            filename = datePath + "/" + filename;
+            //https://on-edu.oss-ap-northeast-1.aliyuncs.com/1.jpg
             ossClient.putObject(bucketname, filename,inputStream);
             ossClient.shutdown();
             //上传之后文件的路径返回
