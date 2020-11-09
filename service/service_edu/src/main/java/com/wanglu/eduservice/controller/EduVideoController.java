3b69853e3dc8849bce8f54334e32a84cd81713a6
wanglu <376638623@qq.com>
2020-11-09 12:58:14 +0900

    '删除小节的后端方法'

--- service/service_edu/src/main/java/com/wanglu/eduservice/controller/EduVideoController.java
+++ service/service_edu/src/main/java/com/wanglu/eduservice/controller/EduVideoController.java
@@ -1,10 +1,11 @@
 package com.wanglu.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.wanglu.commonutils.R;
+import com.wanglu.eduservice.entity.EduVideo;
+import com.wanglu.eduservice.service.EduVideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -15,9 +16,31 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-11-05
  */
 @RestController
-@RequestMapping("/eduservice/edu-video")
+@RequestMapping("/eduservice/video")
 @CrossOrigin
 public class EduVideoController {
+    @Autowired
+    private EduVideoService eduVideoService;
 
+    //添加小节
+    @PostMapping("addVideo")
+    public R addVideo(@RequestBody EduVideo eduVideo) {
+        eduVideoService.save(eduVideo);
+        return R.ok();
+    }
+
+    //删除小节 TODO 删除小节时候删除后面的视频
+    @DeleteMapping("{id}")
+    public R deleteVideo(@PathVariable String id) {
+        eduVideoService.removeById(id);
+        return R.ok();
+    }
+
+    //修改小节
+    @PostMapping("updateVideo")
+    public R updateVideo(@RequestBody EduVideo eduVideo) {
+        eduVideoService.updateById(eduVideo);
+        return R.ok();
+    }
 }
 
