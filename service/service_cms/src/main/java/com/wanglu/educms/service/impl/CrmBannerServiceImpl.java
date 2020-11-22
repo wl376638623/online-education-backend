4cd3ef88496b30add1b6b5b1f205934debdfeb63
wanglu <376638623@qq.com>
2020-11-23 00:01:59 +0900

    '添加redis的方法'

--- service/service_cms/src/main/java/com/wanglu/educms/service/impl/CrmBannerServiceImpl.java
+++ service/service_cms/src/main/java/com/wanglu/educms/service/impl/CrmBannerServiceImpl.java
@@ -5,6 +5,7 @@ import com.wanglu.educms.entity.CrmBanner;
 import com.wanglu.educms.mapper.CrmBannerMapper;
 import com.wanglu.educms.service.CrmBannerService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,12 +22,13 @@ import java.util.List;
 public class CrmBannerServiceImpl extends ServiceImpl<CrmBannerMapper, CrmBanner> implements CrmBannerService {
 
     @Override
+    @Cacheable(key = "'selectIndexList'",value = "banner")
     public List<CrmBanner> selectAllBanner() {
         //根据id进行降序排列 显示排列之后的前两条记录
         QueryWrapper<CrmBanner> wrapper = new QueryWrapper<>();
         wrapper.orderByDesc("id");
         //last方法 拼接sql语句
-        wrapper.last("limit 3");
+        wrapper.last("limit 2");
         List<CrmBanner> list = baseMapper.selectList(null);
         return list;
     }
