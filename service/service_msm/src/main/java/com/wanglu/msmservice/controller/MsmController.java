f8b20956f5b959126f19cf284c2154092848281b
wanglu <376638623@qq.com>
2020-11-24 16:12:27 +0900

    '完成短信发送方法'

--- service/service_msm/src/main/java/com/wanglu/msmservice/controller/MsmController.java
+++ service/service_msm/src/main/java/com/wanglu/msmservice/controller/MsmController.java
@@ -4,10 +4,13 @@ import com.wanglu.commonutils.R;
 import com.wanglu.msmservice.service.MsmService;
 import com.wanglu.msmservice.utils.RandomUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/edumsm/msm")
@@ -16,16 +19,26 @@ public class MsmController {
     @Autowired
     private MsmService msmService;
 
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+
     //发送短息的方法
     @GetMapping("send/{phone}")
     public R sendMsm(@PathVariable String phone) {
+        //从redis上获取验证码 如果获取到直接返回
+        String code = redisTemplate.opsForValue().get(phone);
+        if (!StringUtils.isEmpty(code)) {
+            return R.ok();
+        }
         //生成随机的值传递给阿里云进行发送
-        String code = RandomUtil.getFourBitRandom();
+        code = RandomUtil.getFourBitRandom();
         Map<String, Object> param = new HashMap<>();
         param.put("code", code);
         //调用service中的发送短息方法
         boolean isSend = msmService.send(param, phone);
         if (isSend) {
+            //发送成功，把发送成功的验证码放到redis里面
+            redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
             return R.ok();
         } else {
             return R.error().message("短信发送失败");
