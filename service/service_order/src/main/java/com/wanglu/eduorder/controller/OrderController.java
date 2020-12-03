77b767f31b95e572bc696c37a044f2549be610f4
wanglu <376638623@qq.com>
2020-12-03 23:29:13 +0900

    '添加订单方法'

--- service/service_order/src/main/java/com/wanglu/eduorder/controller/OrderController.java
+++ service/service_order/src/main/java/com/wanglu/eduorder/controller/OrderController.java
@@ -1,9 +1,13 @@
 package com.wanglu.eduorder.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.wanglu.commonutils.JwtUtils;
+import com.wanglu.commonutils.R;
+import com.wanglu.eduorder.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -15,7 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/eduorder/order")
+@CrossOrigin
 public class OrderController {
+    @Autowired
+    private OrderService orderService;
 
+    //1.生成订单方法
+    @PostMapping("createOrder/{courseId}")
+    public R saveOrder(@PathVariable String courseId, HttpServletRequest request) {
+        orderService.createOrders(courseId, JwtUtils.getMemberIdByJwtToken(request));
+        return R.ok();
+    }
 }
 
