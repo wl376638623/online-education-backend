e440db3ffb45ec92febdab57718befbac794621b
wanglu <376638623@qq.com>
2020-12-14 14:26:59 +0900

    '增加微服务模块'

--- service/service_statistics/src/main/java/com/wanglu/staservice/service/impl/StatisticsDailyServiceImpl.java
+++ service/service_statistics/src/main/java/com/wanglu/staservice/service/impl/StatisticsDailyServiceImpl.java
@@ -52,19 +52,19 @@ public class StatisticsDailyServiceImpl extends ServiceImpl<StatisticsDailyMappe
     public Map<String, Object> getShowData(String type, String begin, String end) {
         //根据条件查询对应的数据
         QueryWrapper<StatisticsDaily> wrapper = new QueryWrapper<>();
-        wrapper.between("data_calculated", begin, end);
-        wrapper.select("data_calculated", type);
+        wrapper.between("date_calculated", begin, end);
+        wrapper.select("date_calculated", type);
         List<StatisticsDaily> staList = baseMapper.selectList(wrapper);
         //数据封装 日期 和日期对应的数量
         //前端要求数组结构 对应后端的java代码是list集合
         //创建两个list集合 一个日期list 一个数量list
-        List<String> data_calculated = new ArrayList<>();
+        List<String> date_calculatedList = new ArrayList<>();
         List<Integer> numDataList = new ArrayList<>();
         //遍历查询所有数据list集合 进行封装
         for (int i = 0; i < staList.size(); i++) {
             StatisticsDaily daily = staList.get(i);
             //封装日期list集合
-            data_calculated.add(daily.getDateCalculated());
+            date_calculatedList.add(daily.getDateCalculated());
             //封装数量list集合
             switch (type) {
                 case "login_num":
@@ -85,7 +85,7 @@ public class StatisticsDailyServiceImpl extends ServiceImpl<StatisticsDailyMappe
         }
         //把封装之后的两个list集合放到map集合中 进行返回
         Map<String, Object> map = new HashMap<>();
-        map.put("data_calculated", data_calculated);
+        map.put("date_calculatedList", date_calculatedList);
         map.put("numDataList", numDataList);
         return map;
     }
