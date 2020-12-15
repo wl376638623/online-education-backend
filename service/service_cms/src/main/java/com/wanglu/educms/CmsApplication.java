c645cd61965f9a63a0774ce6bf1fb9dd96a60289
wanglu <376638623@qq.com>
2020-12-15 14:58:29 +0900

    '修改先关方法'

--- service/service_cms/src/main/java/com/wanglu/educms/CmsApplication.java
+++ service/service_cms/src/main/java/com/wanglu/educms/CmsApplication.java
@@ -3,6 +3,7 @@ package com.wanglu.educms;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
